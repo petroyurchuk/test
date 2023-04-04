ee963007e9235597e008f1aada739b36b0e970d4
PetroYurchuk <petroyurchuk66@gmail.com>
2023-04-04 15:55:31 +0300

    cleaned index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import ButtonsCurrency from 'components/ButtonsCurrency/ButtonsCurrency'
 import ProductsList from 'components/Products/ProductsList'
 import TotalPrice from 'components/TotalPrice/TotalPrice'
-import React from 'react'
 
 function App() {
     return (
